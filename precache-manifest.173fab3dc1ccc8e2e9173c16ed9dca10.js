addae2119628b291b1eb7b8182e1f2a2a5048d47
Sergey <levashov04sergey@gmail.com>
2021-08-17 21:22:40 +0300

    Updates

---
+++ precache-manifest.173fab3dc1ccc8e2e9173c16ed9dca10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180ec650d04162061d7970b2c3f4eb3a",
+    "url": "/FireGram/index.html"
+  },
+  {
+    "revision": "66bf9da3b65c186cbf3d",
+    "url": "/FireGram/static/css/main.dce97919.chunk.css"
+  },
+  {
+    "revision": "4a6411f8a10a3aaf326f",
+    "url": "/FireGram/static/js/2.da0996fe.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/FireGram/static/js/2.da0996fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bf9da3b65c186cbf3d",
+    "url": "/FireGram/static/js/main.d1a40ab1.chunk.js"
+  },
+  {
+    "revision": "4b3f81515cdacb7a7016",
+    "url": "/FireGram/static/js/runtime-main.1d10aed1.js"
+  }
+]);
