f050ca0ca3251e812307a06d503a15e917ffa3f4
Sergey <levashov04sergey@gmail.com>
2021-08-17 21:21:09 +0300

    deploy app to github pages

---
+++ build/precache-manifest.4795a8e0ef2b7d6a77fbe03637c64039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940463ce82ed33aa4bf44eabff25c82a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66bf9da3b65c186cbf3d",
+    "url": "/static/css/main.dce97919.chunk.css"
+  },
+  {
+    "revision": "0076bc1fb3c0eef9ad23",
+    "url": "/static/js/2.0e560aae.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/static/js/2.0e560aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bf9da3b65c186cbf3d",
+    "url": "/static/js/main.d1a40ab1.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
